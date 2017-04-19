8d867cb6e5f91a073bbdd0a662dff0e4ba78932e
Jason Sidabras <jsidabra@Jasons-MacBook-Pro.local>
2017-04-19 22:21:23 +0200

    Add the HFSS-test file in order to test connection to the system; MaximizeHField is the GA script using deap

---
+++ HFSS-tests.py
@@ -0,0 +1,63 @@
+# ----------------------------------------------
+# Script Recorded by ANSYS Electronics Desktop Version 2017.0.0
+# 13:58:19  Apr 18, 2017
+# ----------------------------------------------
+from random import *
+import hycohanz as hfss
+from datetime import datetime
+startTime = datetime.now()
+#ScriptEnv.Initialize("Ansoft.ElectronicsDesktop")
+#oDesktop.RestoreWindow()
+#oProject = oDesktop.SetActiveProject("GA_PlanarResonator")
+#oDesign = oProject.SetActiveDesign("HFSSDesign1")
+#oEditor = oDesign.SetActiveEditor("3D Modeler")
+
+[oAnsoftApp, oDesktop] = hfss.setup_interface()
+oProject = hfss.get_active_project(oDesktop)
+oDesign = hfss.set_active_design(oProject, 'HFSSDesign1')
+oEditor = hfss.set_active_editor(oDesign)
+oFieldsReporter = hfss.get_module(oDesign, 'FieldsReporter')
+
+randBinList = lambda n: [randint(0,1) for b in range(1,n+1)]
+
+thing = randBinList(30)
+index = 0
+Vac = []
+Silv = []
+for i in thing:
+    if i == 1:
+        Silv.append("Planar_"+str(index))
+        index += 1
+    else:
+        Vac.append("Planar_"+str(index))
+        index += 1
+
+hfss.assign_material(oEditor, Silv, MaterialName="silver", SolveInside=False)
+hfss.assign_IsModel(oEditor, Vac, MaterialName="silver", SolveInside=False, IsModel=False)
+
+oProject.Save()
+oDesign.Analyze("Setup1")
+hfss.enter_qty(oFieldsReporter, 'H')
+hfss.enter_qty(oFieldsReporter, 'H')
+hfss.calc_op(oFieldsReporter, 'Dot')
+hfss.calc_op(oFieldsReporter, 'Real')
+hfss.enter_vol(oFieldsReporter, 'Sample')
+hfss.calc_op(oFieldsReporter, 'Integrate')
+hfss.clc_eval(
+    oFieldsReporter,
+    'Setup1',
+    'LastAdaptive',
+    9.7e9,
+    0,
+    {},
+)
+out = hfss.get_top_entry_value(
+    oFieldsReporter,
+    'Setup1',
+    'LastAdaptive',
+    9.7e9,
+    0,
+    {},
+)
+print(out[0])
+print(datetime.now() - startTime)
