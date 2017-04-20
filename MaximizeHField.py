74cdaddc0a04bfe2ed6c13856ad14dc7a7ffde09
Jason W. Sidabras <jason.sidabras@cec.mpg.de>
2017-04-20 19:16:13 +0200

    multiple changes. fixed str->float error in GA fitness. Running now fixed email address

--- MaximizeHField.py
+++ MaximizeHField.py
@@ -33,7 +33,7 @@ creator.create("Individual", list, typecode='f', fitness=creator.FitnessMax)
 
 toolbox = base.Toolbox()
 
-# Attribute generator 
+# Attribute generator
 #                      define 'attr_bool' to be an attribute ('gene')
 #                      which corresponds to integers sampled uniformly
 #                      from the range [0,1] (i.e. 0 or 1 with equal
@@ -43,7 +43,7 @@ toolbox.register("attr_bool", random.randint, 0, 1)
 # Structure initializers
 #                         define 'individual' to be an individual
 #                         consisting of 2490 'attr_bool' elements ('genes')
-toolbox.register("individual", tools.initRepeat, creator.Individual, 
+toolbox.register("individual", tools.initRepeat, creator.Individual,
     toolbox.attr_bool, 2490)
 
 # define the population to be a list of individuals
@@ -67,7 +67,7 @@ def evalOneMax(individual):
             Vac.append("Planar_"+str(index))
         index += 1
 
-    if Vac: 
+    if Vac:
     # Check if list is empty
     #    hfss.assign_IsModel(oEditor, Vac, IsModel=False)
         hfss.assign_material(oEditor, Vac, MaterialName="vacuum", SolveInside=True)
@@ -145,25 +145,25 @@ def main():
     # NGEN  is the number of generations for which the
     #       evolution runs
     CXPB, MUTPB, NGEN = 0.5, 0.2, 50
-    
+
     print("Start of evolution")
-    
+
     # Evaluate the entire population
     fitnesses = list(map(toolbox.evaluate, pop))
     for ind, fit in zip(pop, fitnesses):
         ind.fitness.values = fit
-    
+
     print("  Evaluated %i individuals" % len(pop))
-    
+
     # Begin the evolution
     for g in range(NGEN):
         print("-- Generation %i --" % g)
-        
+
         # Select the next generation individuals
         offspring = toolbox.select(pop, len(pop))
         # Clone the selected individuals
         offspring = list(map(toolbox.clone, offspring))
-    
+
         # Apply crossover and mutation on the offspring
         for child1, child2 in zip(offspring[::2], offspring[1::2]):
 
@@ -182,33 +182,33 @@ def main():
             if random.random() < MUTPB:
                 toolbox.mutate(mutant)
                 del mutant.fitness.values
-    
+
         # Evaluate the individuals with an invalid fitness
         invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
         fitnesses = map(toolbox.evaluate, invalid_ind)
         for ind, fit in zip(invalid_ind, fitnesses):
             ind.fitness.values = fit
-        
+
         print("  Evaluated %i individuals" % len(invalid_ind))
-        
+
         # The population is entirely replaced by the offspring
         pop[:] = offspring
-        
+
         # Gather all the fitnesses in one list and print the stats
         fits = [ind.fitness.values[0] for ind in pop]
-        
+
         length = len(pop)
         mean = sum(fits) / length
         sum2 = sum(x*x for x in fits)
         std = abs(sum2 / length - mean**2)**0.5
-        
+
         print("  Min %s" % min(fits))
         print("  Max %s" % max(fits))
         print("  Avg %s" % mean)
         print("  Std %s" % std)
-    
+
     print("-- End of (successful) evolution --")
-    
+
     best_ind = tools.selBest(pop, 1)[0]
     print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
     print(datetime.now() - startTime)
