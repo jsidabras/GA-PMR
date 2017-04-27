6e6346948c10960c661e2d6a32f9ba1f177b25ff
Jason W. Sidabras <jason.sidabras@cec.mpg.de>
2017-04-27 14:37:13 +0200

    clean up and ensure it loads a test file correctly

--- HFSS-loadbest.py
+++ HFSS-loadbest.py
@@ -7,8 +7,6 @@
 from random import *
 import argparse
 import hycohanz as hfss
-from datetime import datetime
-startTime = datetime.now()
 
 [oAnsoftApp, oDesktop] = hfss.setup_interface()
 oProject = hfss.get_active_project(oDesktop)
@@ -24,10 +22,18 @@ f = open(args.file, 'r')
 thing = f.readline()
 f.close()
 
+thing = []
+for i in range(1,len(loadthing),2):
+    out = loadthing[i]
+    thing.extend(out)
+
+# Drop /n from the end of the list
+thing.pop()
+
 Vac = []
 Silv = []
 for i in thing:
-    if i == 1:
+    if i == '1' or i == 1:
         Silv.append("Elm_"+str(index))
         index += 1
     else:
@@ -35,45 +41,19 @@ for i in thing:
         index += 1
 
 oDesktop.ClearMessages("", "", 3)
-    
-if Vac: 
+
 # Check if list is empty
-    # hfss.assign_White(oEditor, Silv)
+if Vac:
+    hfss.assign_White(oEditor, Silv)
     hfss.assign_material(oEditor, Vac, MaterialName="vacuum", SolveInside=True)
 if Silv:
-    # hfss.assign_Orange(oEditor, Silv)
-   hfss.assign_material(oEditor, Silv, MaterialName="silver", SolveInside=False)
+    hfss.assign_Orange(oEditor, Silv)
+    hfss.assign_material(oEditor, Silv, MaterialName="silver", SolveInside=False)
 
-# oProject.Save()
 try:
     oDesign.Analyze("Setup1")
 except:
     print("Simulation Error Set Fitness to 0")
-    # return 0,
+    return 0,
 
-oFieldsReporter.CalcStack('clear')
-hfss.enter_qty(oFieldsReporter, 'H')
-hfss.enter_qty(oFieldsReporter, 'H')
-hfss.calc_op(oFieldsReporter, 'Conj')
-hfss.calc_op(oFieldsReporter, 'Dot')
-hfss.calc_op(oFieldsReporter, 'Real')
-hfss.enter_vol(oFieldsReporter, 'Sample')
-hfss.calc_op(oFieldsReporter, 'Integrate')
-hfss.clc_eval(
-    oFieldsReporter,
-    'Setup1',
-    'LastAdaptive',
-    9.7e9,
-    0,
-    {},
-)
-out = hfss.get_top_entry_value(
-    oFieldsReporter,
-    'Setup1',
-    'LastAdaptive',
-    9.7e9,
-    0,
-    {},
-)
-print(out[0])
-print(datetime.now() - startTime)
+oProject.Save()
